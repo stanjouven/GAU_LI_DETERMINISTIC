04d7f28f59a5b823cdbcef3f951e765f68f11db8
stanislas jouven <stanislasjouven@gmail.com>
2019-05-19 18:34:47 +0200

    do dijkstra algorithm to compute every paths needed for the source estimation method

--- test_source_loc.py
+++ test_source_loc.py
@@ -13,10 +13,16 @@ def ptva_li(graph, obs_time, distribution) :
     source = 3
     mu = distribution.mean()
     sigma = distribution.std()
+    obs = np.array(list(obs_time.keys()))
+
+    path_lengths = {}
+    paths = {}
+    for o in obs:
+        path_lengths[o], paths[o] = nx.single_source_dijkstra(graph, o)
 
     ### Run the estimation
     s_est, likelihoods, d_mu, cov = se.ml_estimate(graph, obs_time, sigma, mu, paths,
-        path_lengths, max_dist=max_dist)
+        path_lengths)
 
     ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=True)
 
