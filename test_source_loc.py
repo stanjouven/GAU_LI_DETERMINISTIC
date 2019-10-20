056c8b522c5ae940e7cff36df0d1a51b46d09da4
stanislas jouven <stanislasjouven@gmail.com>
2019-10-20 22:23:44 +0200

    delete prints

--- test_source_loc.py
+++ test_source_loc.py
@@ -11,9 +11,7 @@ from termcolor import colored
 ### Compute a batch in parallel
 def ptva_li(graph, obs_time, distribution) :
     mu = distribution.mean()
-    print('mu ', mu)
     sigma = distribution.std()
-    print('sigma ', sigma)
     obs = np.array(list(obs_time.keys()))
 
     path_lengths = {}
